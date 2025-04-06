5d2d9652d84a0fea5f2b1e909ea9a4f2fd7f167f
Juan Pablo <juan.pablo656@gmail.com>
2025-04-06 10:55:41 -0400

    h

--- bot.php
+++ bot.php
@@ -17,18 +17,23 @@ function sendMessage($chat_id, string $response)
 
 switch ($message) {
     case "/start":
-        $response = "Bienvenido a ShopBot!\n\nPara Comenzar, por favor ingrese el producto que desea buscar.";
+        $response = "Bienvenido a ShopBot!\n\nPara Comenzar, por favor ingrese /productos para ver los productos disponibles.";
         sendMessage($chat_id, $response);
         break;
 
-    case "/products":
+    case "/productos":
         $response = "Productos disponibles:\n1. /Carne A\n2. /Queso\n3. /Jamón\n4. Cereal\n5. Leche\n6. Yogur\n7. Bebidas\n8. Jugos\n9. Pan\n10. Pasteles\n11. Tortas\n12. Detergente\n13. Lavaloza";
+        sendMessage($chat_id, $response);
         break;
-        case "/Carne" || "/Queso" || "Jamon":
+
+   case "/Carne" || "/Queso" || "Jamon":
         $response = "El producto que seleccionaste se encuentra en el pasillo 1";
+        sendMessage($chat_id, $response);
         break;
+
     case "/help":
         $response = "Available commands:\n/start - Inicia el bot\n/help - Muestra la ayuda\n/products - Muestra los productos disponibles";
+        sendMessage($chat_id, $response);
         break;
 
     default:
