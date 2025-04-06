686f30c312f18f931ed6a6d5d0431813566fb199
Juan Pablo <juan.pablo656@gmail.com>
2025-04-06 11:33:34 -0400

    h

--- bot.php
+++ bot.php
@@ -18,24 +18,18 @@ function sendMessage($chat_id, string $response)
 
 if ($message == "/start") {
     $response = "Welcome to the bot! Use /help to see available commands.";
-    sendMessage($chat_id, $response);
 } elseif ($message == "/help") {
     $response = "Available commands:\n/start - Start the bot\n/help - Show this help message";
-    sendMessage($chat_id, $response);
 } elseif ($message == "/productos") {
     $response = "Here are the available products:\n1. /Product A\n2. /Product B\n3. /Product C";
-    sendMessage($chat_id, $response);
 } elseif ($message == "/Product A") {
     $response = "Product A is a great choice! It costs $10.";
-    sendMessage($chat_id, $response);
 } elseif ($message == "/Product B") {
     $response = "Product B is a great choice! It costs $20.";
-    sendMessage($chat_id, $response);
 } elseif ($message == "/Product C") {
     $response = "Product C is a great choice! It costs $30.";
-    sendMessage($chat_id, $response);
 } else {
     $response = "Unknown command. Use /help to see available commands.";
-    sendMessage($chat_id, $response);
 }
+sendMessage($chat_id, $response);
 
