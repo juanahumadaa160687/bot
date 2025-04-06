6b9abebc91190aba672327f532453e515ffdd024
Juan Pablo <juan.pablo656@gmail.com>
2025-04-06 11:00:42 -0400

    h

--- bot.php
+++ bot.php
@@ -17,27 +17,27 @@ function sendMessage($chat_id, string $response)
 
 switch ($message) {
     case "/start":
-        $response = "Bienvenido a ShopBot!\n\nPara Comenzar, por favor ingrese /productos para ver los productos disponibles.";
+        $response = "Bienvenido a ShopBot!\n\nPara Comenzar, por favor escriba el producto que desea consultar o escriba /productos para ver los productos disponibles.";
         sendMessage($chat_id, $response);
         break;
 
     case "/productos":
-        $response = "Productos disponibles:\n1. /Carne A\n2. /Queso\n3. /Jamón\n4. Cereal\n5. Leche\n6. Yogur\n7. Bebidas\n8. Jugos\n9. Pan\n10. Pasteles\n11. Tortas\n12. Detergente\n13. Lavaloza";
+        $response = "Productos disponibles:\n1. Carne \n2. Queso\n3. Jamon\n4. Cereal\n5. Leche\n6. Yogur\n7. Bebidas\n8. Jugos\n9. Pan\n10. Pasteles\n11. Tortas\n12. Detergente\n13. Lavaloza";
         sendMessage($chat_id, $response);
         break;
 
-   case "/Carne" || "/Queso" || "Jamon":
-        $response = "El producto que seleccionaste se encuentra en el pasillo 1";
+   case "Carne" || "Queso" || "Jamon":
+        $response = "El producto que seleccionaste se encuentra en el pasillo 1. \n\nSi deseas ver más productos, escribe /productos.";
         sendMessage($chat_id, $response);
         break;
 
-    case "/help":
-        $response = "Available commands:\n/start - Inicia el bot\n/help - Muestra la ayuda\n/products - Muestra los productos disponibles";
+    case "/ayuda":
+        $response = "Opciones disponibles:\n/start - Inicia el bot\n/ayuda - Muestra la ayuda\n/productos - Muestra los productos disponibles";
         sendMessage($chat_id, $response);
         break;
 
     default:
-        $response = "No entiendo lo que dices.\n\nEscribe /help para ver los comandos disponibles.";
+        $response = "No entiendo lo que dices.\n\nEscribe /ayuda para ver los comandos disponibles.";
         break;
 }
 
