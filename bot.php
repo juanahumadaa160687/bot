49403c990aa27c444fddf7c7518a11f891696344
Juan Pablo <juan.pablo656@gmail.com>
2025-04-06 11:50:59 -0400

    h

--- bot.php
+++ bot.php
@@ -17,21 +17,23 @@ function sendMessage($chat_id, string $response)
 }
 
 if ($message == "/start") {
-    $response = "Bienvenido a ShopBot. Usa /productos para ver la lista de productos.";
+    $response = "Bienvenido a ShopBot. Escribe el producto que buscar o usa /productos para ver la lista de productos.";
 } elseif ($message == "/ayuda") {
-    $response = "Comandos disponibles:\n/start - Inicia la conversación\n/help Muestra el menú de ayuda\n/productos  Muestra la lista de productos";
+    $response = "Comandos disponibles:\n/start - Inicia la conversación\n/ayuda Muestra el menú de ayuda\n/productos  Muestra la lista de productos";
 } elseif ($message == "/productos") {
     $response = "Estos son nuestros productos:\n\n1. /carne \n2. /queso \n3. /jamon \n4 /leche \n5 /yogurt \n6 /cereal \n7 /bebidas \n8 /jugos \n9 /pan \n10 /pasteles \n11 /tortas \n12 /detergente \n13 /lavaloza \n\nUsa el nombre del producto para ver su ubicación.";
-} elseif ($message == "/carne" || $message == "/queso" || $message == "/jamon") {
-    $response = "El producto que has elegido está en el pasillo 1.";
-} elseif ($message == "/leche" || $message == "/yogurt" || $message == "/cereal") {
-    $response = "El producto que has elegido está en el pasillo 2.";
-} elseif ($message == "/bebidas" || $message == "/jugos") {
-    $response = "El producto que has elegido está en el pasillo 3.";
-} elseif ($message == "/pan" || $message == "/pasteles" || $message == "/tortas") {
-    $response = "El producto que has elegido está en el pasillo 4.";
-} elseif ($message == "/detergente" || $message == "/lavaloza") {
-    $response = "El producto que has elegido está en el pasillo 5.";
+
+} elseif ($message == "/carne" || $message == "/queso" || $message == "/jamon" || $message == "carne" || $message == "queso" || $message == "jamon") {
+    $response = "El producto que has elegido está en el pasillo 1. \n\nSi necesitas ver más productos, usa /productos.";
+} elseif ($message == "/leche" || $message == "/yogurt" || $message == "/cereal" || $message == "leche" || $message == "yogurt" || $message == "cereal") {
+    $response = "El producto que has elegido está en el pasillo 2. \n\nSi necesitas ver más productos, usa /productos.";
+} elseif ($message == "/bebidas" || $message == "/jugos" || $message == "bebidas" || $message == "jugos") {
+    $response = "El producto que has elegido está en el pasillo 3. \n\nSi necesitas ver más productos, usa /productos.";
+} elseif ($message == "/pan" || $message == "/pasteles" || $message == "/tortas" || $message == "pan" || $message == "pasteles" || $message == "tortas") {
+    $response = "El producto que has elegido está en el pasillo 4. \n\nSi necesitas ver más productos, usa /productos.";
+} elseif ($message == "/detergente" || $message == "/lavaloza" || $message == "detergente" || $message == "lavaloza") {
+    $response = "El producto que has elegido está en el pasillo 5. \n\nSi necesitas ver más productos, usa /productos.";
+
 } else {
     $response = "No entiendo tu pregunta. Usa /ayuda para ver los comandos disponibles.";
 }
