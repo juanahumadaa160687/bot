905a4e9adda16fc0ff8a3bceb9adc9c667be51ee
Juan Pablo <juan.pablo656@gmail.com>
2025-04-06 10:38:48 -0400

    h

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ $message = $update->message->text;
 
 if (strpos($message, "/start") === 0) {
     $location = substr($message, 9);
-    $weather = json_decode(file_get_contents("https://apí.openweathermap.or/data/2.5/weather?q=".$location."&appid=f3a0b8c2d4e5f1a7c6e9b8f1a7c6e9b8&units=metric"), true)[$weather][0]["main"];
+    $weather = json_decode(file_get_contents("https://apí.openweathermap.or/data/2.5/weather?q=".$location."&appid=f3a0b8c2d4e5f1a7c6e9b8f1a7c6e9b8&units=metric"), true)["weather"][0]["main"];
     file_get_contents($path."/sendMessage?chat_id=".$chat_id."&text=Weather in ".$location.": ".$weather);
 }
 
