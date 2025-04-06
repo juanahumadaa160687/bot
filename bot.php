f9958b1c18813428ec4ebc25ea5ef82c1a671055
Juan Pablo <juan.pablo656@gmail.com>
2025-04-06 10:50:45 -0400

    h

--- bot.php
+++ bot.php
@@ -8,13 +8,31 @@ $update = json_decode(file_get_contents("php://input"));
 $chat_id = $update->message->chat->id;
 $message = $update->message->text;
 
-if ($message == "/start") {
-    $response = "Welcome to the bot! Use /help to see available commands.";
-} elseif ($message == "/help") {
-    $response = "Available commands:\n/start - Start the bot\n/help - Show this help message";
-} else {
-    $response = "I don't understand that command.";
+function sendMessage($chat_id, string $response)
+{
+    global $path;
+    $url = $path . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($response);
+    file_get_contents($url);
 }
-file_get_contents($path . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($response));
 
+switch ($message) {
+    case "/start":
+        $response = "Bienvenido a ShopBot!\n\nPara Comenzar, por favor ingrese el producto que desea buscar.";
+        sendMessage($chat_id, $response);
+        break;
+
+    case "/products":
+        $response = "Productos disponibles:\n1. /Carne A\n2. /Queso\n3. /Jamón\n4. Cereal\n5. Leche\n6. Yogur\n7. Bebidas\n8. Jugos\n9. Pan\n10. Pasteles\n11. Tortas\n12. Detergente\n13. Lavaloza";
+        break;
+        case "/Carne" || "/Queso" || "Jamon":
+        $response = "El producto que seleccionaste se encuentra en el pasillo 1";
+        break;
+    case "/help":
+        $response = "Available commands:\n/start - Inicia el bot\n/help - Muestra la ayuda\n/products - Muestra los productos disponibles";
+        break;
+
+    default:
+        $response = "Unknown command. Use /help to see available commands.";
+        break;
+}
 
