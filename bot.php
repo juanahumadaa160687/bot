ea6c3d333251e0e99c8e4d3ad2d38b624e491704
Juan Pablo <juan.pablo656@gmail.com>
2025-04-06 11:44:21 -0400

    h

--- bot.php
+++ bot.php
@@ -17,15 +17,21 @@ function sendMessage($chat_id, string $response)
 }
 
 if ($message == "/start") {
-    $response = "Welcome to the bot! Use /help to see available commands.";
-} elseif ($message == "/help") {
-    $response = "Available commands:\n/start - Start the bot\n/help - Show this help message";
+    $response = "Bienvenido a ShopBot. Usa /productos para ver la lista de productos.";
+} elseif ($message == "/ayuda") {
+    $response = "Comandos disponibles:\n/start - Inicia la conversación\n/help Muestra el menú de ayuda\n/productos  Muestra la lista de productos";
 } elseif ($message == "/productos") {
-    $response = "Here are the available products:\n1. /ProductA\n2. /ProductB\n3. /ProductC";
-} elseif ($message == "/ProductA" || $message == "/ProductB") {
-    $response = "Product A is a great choice! It costs $10.";
-} elseif ($message == "/ProductC") {
-    $response = "Product C is a great choice! It costs $30.";
+    $response = "Estos son nuestros productos:\n\n1. /carne \n2. /queso \n3. /jamon \n4 /leche \n5 /yogurt \n6 /cereal \n7 /bebidas \n8 /jugos \n9 /pan \n10 /pasteles \n11 /tortas \n12 /detergente \n13 /lavaloza \n\nUsa el nombre del producto para ver su ubicación.";
+} elseif ($message == "/carne" || $message == "/queso" || $message == "/jamon") {
+    $response = "El producto que has elegido está en el pasillo 1.";
+} elseif ($message == "/leche" || $message == "/yogurt" || $message == "/cereal") {
+    $response = "El producto que has elegido está en el pasillo 2.";
+} elseif ($message == "/bebidas" || $message == "/jugos") {
+    $response = "El producto que has elegido está en el pasillo 3.";
+} elseif ($message == "/pan" || $message == "/pasteles" || $message == "/tortas") {
+    $response = "El producto que has elegido está en el pasillo 4.";
+} elseif ($message == "/detergente" || $message == "/lavaloza") {
+    $response = "El producto que has elegido está en el pasillo 5.";
 } else {
     $response = "Unknown command. Use /help to see available commands.";
 }
