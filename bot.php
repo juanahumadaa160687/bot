d56c1d034fa206b56fea6ebe3c4d88c6edda48e7
Juan Pablo <juan.pablo656@gmail.com>
2025-04-07 19:29:15 -0400

    h

--- bot.php
+++ bot.php
@@ -23,14 +23,14 @@ if ($message == "/start") {
 } elseif ($message == "/productos") {
     $response = "Estos son nuestros productos:\n\n1. /carne \n2. /queso \n3. /jamon \n4 /leche \n5 /yogurt \n6 /cereal \n7 /bebidas \n8 /jugos \n9 /pan \n10 /pasteles \n11 /tortas \n12 /detergente \n13 /lavaloza \n\nUsa el nombre del producto para ver su ubicación.";
 
-} elseif ($message == "/carne" || $message == "/queso" || $message == "/jamon" || strtolower($message) == "Carne" || strtolower($message) == "queso" || strtolower($message) == "jamon") {
+} elseif ($message == "/carne" || $message == "/queso" || $message == "/jamon" || $message == strtolower("Carne") || $message == strtolower("Queso") || $message == strtolower("Jamon")) {
     $response = "El producto que has elegido está en el pasillo 1. \n\nSi necesitas ver más productos, usa /productos.";
-} elseif ($message == "/leche" || $message == "/yogurt" || $message == "/cereal" || strtolower($message) == "Cereal" || strtolower($message) == "Leche" || strtolower($message) == "Yogurt") {
+} elseif ($message == "/leche" || $message == "/yogurt" || $message == "/cereal" || $message == strtolower("Cereal") || $message == strtolower("Leche") || $message == strtolower("Yogurt")) {
     $response = "El producto que has elegido está en el pasillo 2. \n\nSi necesitas ver más productos, usa /productos.";
-} elseif ($message == "/bebidas" || $message == "/jugos" || strtolower($message) == "Jugos" || strtolower($message) == "Bebidas")
+} elseif ($message == "/bebidas" || $message == "/jugos" || $message == strtolower("Bebidas") || $message == strtolower("Jugos"))
 {
     $response = "El producto que has elegido está en el pasillo 3. \n\nSi necesitas ver más productos, usa /productos.";
-} elseif ($message == "/pan" || $message == "/pasteles" || $message == "/tortas" || $message == strtolower("Pan") || $message == strtolower("Pasteles") || $message == strtolower("Tortas")) {
+} elseif ($message == "/pan" || $message == "/pasteles" || $message == "/tortas" || $message == strtolower("Pan") ||$message == strtolower("Pasteles") || $message == strtolower("Tortas")) {
     $response = "El producto que has elegido está en el pasillo 4. \n\nSi necesitas ver más productos, usa /productos.";
 } elseif ($message == "/detergente" || $message == "/lavaloza" || $message == strtolower("Detergente") || $message == strtolower("Lavaloza")) {
     $response = "El producto que has elegido está en el pasillo 5. \n\nSi necesitas ver más productos, usa /productos.";
