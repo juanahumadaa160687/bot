d95bd9df2c24f53e2d4cde084f498fbcdc4b305b
Juan Pablo <juan.pablo656@gmail.com>
2025-04-07 11:33:10 -0400

    h

--- bot.php
+++ bot.php
@@ -17,23 +17,24 @@ function sendMessage($chat_id, string $response)
 }
 
 if ($message == "/start") {
-    $response = "Bienvenido a ShopBot. Escribe el producto que buscar o usa /productos para ver la lista de productos.";
+    $response = "Bienvenido a ShopBot. Escribe el producto que deseas buscar o usa /productos para ver la lista de productos.";
 } elseif ($message == "/ayuda") {
-    $response = "Comandos disponibles:\n/start - Inicia la conversación\n/ayuda Muestra el menú de ayuda\n/productos  Muestra la lista de productos";
+    $response = "Comandos disponibles:\n1.- /start - Inicia la conversación\n2.- /ayuda Muestra el menú de ayuda\n3.- /productos  Muestra la lista de productos\n4.- /salir - Termina la conversación\nPara buscar un producto, simplemente escribe su nombre o usa el comando correspondiente. \n\nEjemplo: Carne, Queso, Leche, /Carne, /Queso etc.";
 } elseif ($message == "/productos") {
     $response = "Estos son nuestros productos:\n\n1. /carne \n2. /queso \n3. /jamon \n4 /leche \n5 /yogurt \n6 /cereal \n7 /bebidas \n8 /jugos \n9 /pan \n10 /pasteles \n11 /tortas \n12 /detergente \n13 /lavaloza \n\nUsa el nombre del producto para ver su ubicación.";
 
-} elseif ($message == "/carne" || $message == "/queso" || $message == "/jamon" || $message == "carne" || $message == "queso" || $message == "jamon") {
+} elseif ($message == "/carne" || $message == "/queso" || $message == "/jamon" || $message == strtolower("Carne") || $message == strtolower("Queso") || $message == strtolower("Jamon")) {
     $response = "El producto que has elegido está en el pasillo 1. \n\nSi necesitas ver más productos, usa /productos.";
-} elseif ($message == "/leche" || $message == "/yogurt" || $message == "/cereal" || $message == "leche" || $message == "yogurt" || $message == "cereal") {
+} elseif ($message == "/leche" || $message == "/yogurt" || $message == "/cereal" || $message == strtolower("Leche") || $message == strtolower("Yogurt") || $message == strtolower("Cereal")) {
     $response = "El producto que has elegido está en el pasillo 2. \n\nSi necesitas ver más productos, usa /productos.";
-} elseif ($message == "/bebidas" || $message == "/jugos" || $message == "bebidas" || $message == "jugos") {
+} elseif ($message == "/bebidas" || $message == "/jugos" || $message == strtolower("Bebidas") || $message == strtolower("Jugos")) {
     $response = "El producto que has elegido está en el pasillo 3. \n\nSi necesitas ver más productos, usa /productos.";
-} elseif ($message == "/pan" || $message == "/pasteles" || $message == "/tortas" || $message == "pan" || $message == "pasteles" || $message == "tortas") {
+} elseif ($message == "/pan" || $message == "/pasteles" || $message == "/tortas" || $message == strtolower("Pan") || $message == strtolower("Pasteles") || $message == strtolower("Tortas")) {
     $response = "El producto que has elegido está en el pasillo 4. \n\nSi necesitas ver más productos, usa /productos.";
-} elseif ($message == "/detergente" || $message == "/lavaloza" || $message == "detergente" || $message == "lavaloza") {
+} elseif ($message == "/detergente" || $message == "/lavaloza" || $message == strtolower("Detergente") || $message == strtolower("Lavaloza")) {
     $response = "El producto que has elegido está en el pasillo 5. \n\nSi necesitas ver más productos, usa /productos.";
-
+}elseif ($message == "/salir"){
+    $response = "Gracias por usar ShopBot. ¡Hasta luego!";
 } else {
     $response = "No entiendo tu pregunta. Usa /ayuda para ver los comandos disponibles.";
 }
